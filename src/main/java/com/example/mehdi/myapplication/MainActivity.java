9d58d3317dcd46b35db134d7fc204095d9051d55
Gergy21 <39582355+Gergy21@users.noreply.github.com>
2018-05-24 09:28:36 +0200

    Add files via upload

---
+++ src/main/java/com/example/mehdi/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mehdi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+
+
+
+
+
+
+public class MainActivity extends AppCompatActivity
+{
+    Button buttonscan;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
