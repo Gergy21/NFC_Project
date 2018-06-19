c17365526c703da9fcc294eca5680536bad633eb
Gergy21 <39582355+Gergy21@users.noreply.github.com>
2018-06-19 15:57:00 +0200

    Add files via upload

--- src/main/java/com/example/mehdi/myapplication/MainActivity.java
+++ src/main/java/com/example/mehdi/myapplication/MainActivity.java
@@ -2,27 +2,26 @@ package com.example.mehdi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-
-
-
-
-
-
-
-public class MainActivity extends AppCompatActivity
-{
-    Button buttonscan;
-
-
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Button Scaner = (Button) findViewById(R.id.scanButton);
+        final ImageView UpArrow = (ImageView) findViewById(R.id.fleche);
+        UpArrow.setVisibility(View.INVISIBLE);
+
+        Scaner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpArrow.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
